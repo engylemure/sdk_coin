f9b4fc826211c56cb002b69fa28f8a413a1ecea1
jordao <jordao.rosario01@gmail.com>
2017-09-13 21:06:36 -0300

    1.0

--- sdk/graph_example.php
+++ sdk/graph_example.php
@@ -1,57 +1,38 @@
-<?php
-require_once("bc_ticker.php");
-require_once ("jpgraph/src/jpgraph.php");
-require_once ("jpgraph/src/jpgraph_line.php");
-echo "ok";
-$datay1 = array();
-$datay2 = array();
-$datax = array();
-$ticker = new Ticker();
-$day_unix = 3600*24;
-$ticker->createInstance();
-$array_ticker = $ticker->getByPeriod($ticker->getDateUnix()-$day_unix,$ticker->getDateUnix(),3600);
-foreach($array_ticker as $k => $var){
-    array_push($datay1, $var->getHigh());
-    array_push($datay2, $var->getLow());
-    array_push($datax, $var->getDate());
-}
-// Setup the graph
-$graph = new Graph(600,500);
-$graph->SetScale("textlin");
+<html>
+  <head>
+  <?php
+        require_once("bc_ticker.php");
+        $dataPoints = array();
+        array_push($dataPoints,array('Date','High','Low'));
+        $ticker = new Ticker();
+        $ticker->createInstance();
+        $array_ticker = $ticker->getByPeriod($ticker->getDateUnix()-3600*48,$ticker->getDateUnix(),30);
+        foreach($array_ticker as $k => $var){
+            array_push($dataPoints, array($var->getDate(), $var->getHigh(),$var->getLow()));
+        }
+    ?>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      
+      
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable(<?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>);
 
-$theme_class=new UniversalTheme;
+        var options = {
+          title: 'Bitcoin Quotation',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
 
-$graph->SetTheme($theme_class);
-$graph->img->SetAntiAliasing(false);
-$graph->title->Set('BitCoin Graph Example - With Bc_Ticker');
-$graph->SetBox(false);
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 
-$graph->img->SetAntiAliasing();
-
-$graph->yaxis->HideZeroLabel();
-$graph->yaxis->HideLine(false);
-$graph->yaxis->HideTicks(false,false);
-
-$graph->xgrid->Show();
-$graph->xgrid->SetLineStyle("solid");
-$graph->xaxis->SetTickLabels($datax);
-$graph->xgrid->SetColor('#E3E3E3');
-
-// Create the first line
-$p1 = new LinePlot($datay1);
-$graph->Add($p1);
-$p1->SetColor("#6495ED");
-$p1->SetLegend('High');
-
-// Create the second line
-$p2 = new LinePlot($datay2);
-$graph->Add($p2);
-$p2->SetColor("#B22222");
-$p2->SetLegend('Low');
-
-
-$graph->legend->SetFrameWeight(1);
-
-// Output line
-$graph->Stroke();
-?>
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="curve_chart" style="width: 900px; height: 500px"></div>
+  </body>
+</html>
